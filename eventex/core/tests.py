3b4edb5a7eb4b914717915c33be41d31f35b938d
luiz <luizmj@gmail.com>
2016-03-01 10:07:52 -0300

    Habilita inscrição

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -12,3 +12,6 @@ class HomeTest(TestCase):
     def test_template(self):
         '''Must use with index.html'''
         self.assertTemplateUsed(self.resp, 'index.html')
+
+    def test_subscription_link(self):
+        self.assertContains(self.resp, 'href="/inscricao/"')
