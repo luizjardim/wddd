c3bbe6ea5011cb012c3fd04bd06c1a3c09dab17d
luiz <luizmj@gmail.com>
2016-03-15 17:14:16 -0300

    add admin

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -20,3 +20,6 @@ class SubscriptionModelTest(TestCase):
     def test_create_at(self):
         """Subscription must have an auto created_at attr."""
         self.assertIsInstance(self.obj.created_at, datetime)
+
+    def test_str(self):
+        self.assertEqual('Luiz Felipe', str(self.obj))
